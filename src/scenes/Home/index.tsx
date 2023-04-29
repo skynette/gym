c1724f500465d5625bb6bbe67dae67f3c0dfb29e
Joshua <cutejosh2@gmail.com>
2023-04-30 00:14:57 +0100

    added framer motion to homepage divs

--- src/scenes/Home/index.tsx
+++ src/scenes/Home/index.tsx
@@ -7,6 +7,7 @@ import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import { motion } from "framer-motion";
 
 type Props = {
 	setSelectedPage: (page: SelectedPage) => void;
@@ -21,11 +22,24 @@ const Home = ({ setSelectedPage }: Props) => {
 			className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0"
 		>
 			{/* IMAGE AND MAIN HEADER */}
-			<div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
+			<motion.div
+				onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+				className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6"
+			>
 				{/* MAIN HEADER */}
 				<div className="z-10 mt-32 md:basis-3/5">
 					{/* HEADINGS */}
-					<div className="md:-mt-20">
+					<motion.div
+						className="md:-mt-20"
+						initial="hidden"
+						whileInView="visible"
+						viewport={{ once: true, amount: 0.5 }}
+						transition={{ duration: 0.5 }}
+						variants={{
+							hidden: { opacity: 0, x: -50 },
+							visible: { opacity: 1, x: 0 }
+						}}
+					>
 						<div className="relative">
 							<div className="before:absolute before:-top-20  before:-left-20 before:z-[-1] md:before:content-evolvetext">
 								<img alt="home-page-text" src={HomePageText} />
@@ -35,10 +49,20 @@ const Home = ({ setSelectedPage }: Props) => {
 						<p className="mt-8 text-sm">
 							We are a community of like-minded individuals who are passionate about learning and growing together. We are a community of like-minded individuals who are passionate about learning and growing together.
 						</p>
-					</div>
+					</motion.div>
 
 					{/* ACTION BUTTON */}
-					<div className="mt-8 flex items-center gap-8">
+					<motion.div
+						className="mt-8 flex items-center gap-8"
+						initial="hidden"
+						whileInView="visible"
+						viewport={{ once: true, amount: 0.5 }}
+						transition={{ duration: 0.5, delay: 0.2 }}
+						variants={{
+							hidden: { opacity: 0, x: -50 },
+							visible: { opacity: 1, x: 0 }
+						}}
+					>
 						<ActionButton setSelectedPage={setSelectedPage}>Join</ActionButton>
 						<AnchorLink
 							className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
@@ -47,14 +71,14 @@ const Home = ({ setSelectedPage }: Props) => {
 						>
 							<p>Learn More</p>
 						</AnchorLink>
-					</div>
+					</motion.div>
 				</div>
 
 				{/* IMAGE */}
 				<div className="flex basis-3/5 justify-center md:z-10 md:ml-40 mt:-16 md:justify-end">
 					<img alt="home-page-graphic" src={HomePageGraphic} />
 				</div>
-			</div>
+			</motion.div>
 
 			{/* SPONSORS */}
 			{isAboveMediumScreens && (
